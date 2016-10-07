c492deb1b70417e09cc95fcd976c5489ce768986
fardanoleg <fardanoleg@gmail.com>
2016-10-07 12:33:37 -0700

    updated

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 var app = angular.module('myApp', []);
-
 app.controller("myController", function ($http) {
-
     var self = this;
     this.data = {};
     this.studentArray = [];
@@ -9,7 +7,6 @@ app.controller("myController", function ($http) {
     this.studentCourse = "";
     this.studentGrade = "";
     this.average = 0;
-
     this.addClicked = function () {                               //add new student to the database
         if (!isNaN(self.studentName) || isNaN(self.studentGrade)) {
             console.log("student name is incorrect or student grade is incorrect");
@@ -25,32 +22,9 @@ app.controller("myController", function ($http) {
                 grade: self.studentGrade
             };
         }
-        console.log(self.data);
-
         this.addStudentToDataBase();
         this.cancelClicked();
     };
-
-    this.getDataServer = function () {                            //get data from the server
-        $http({
-            datatype: "json",
-            data: $.param({api_key: "Yd2V1lB6e5"}),
-            method: "post",
-            url: 'http://s-apis.learningfuze.com/sgt/get',
-            cache: false,
-            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-        })
-            .then(
-                function (response) {
-                    console.log(response);
-                    console.log(response.data.data);
-                    self.studentArray = response.data.data;
-                    console.log(self.studentArray);
-                    self.calculateAverage();
-                });
-
-    };
-
     this.addStudentToDataBase = function () {                //add student to the database on the server
         $http({
             datatype: "json",
@@ -73,9 +47,23 @@ app.controller("myController", function ($http) {
                     self.calculateAverage();
                 })
     };
-
+    this.getDataServer = function () {                            //get data from the server
+        $http({
+            datatype: "json",
+            data: $.param({api_key: "Yd2V1lB6e5"}),
+            method: "post",
+            url: 'http://s-apis.learningfuze.com/sgt/get',
+            cache: false,
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+        })
+            .then(
+                function (response) {
+                    console.log(response);
+                    self.studentArray = response.data.data;
+                    self.calculateAverage();
+                });
+    };
     this.deletStudentFromDataBase = function (index) {              // delet student from the databse, from the server
-        console.log('deletinggggg', index);
         var item = self.studentArray[index];
         console.log('item', item);
         $http({
@@ -88,7 +76,6 @@ app.controller("myController", function ($http) {
                 api_key: "Yd2V1lB6e5",
                 student_id: item.id
             })
-
         })
             .then(
                 function (response) {
@@ -98,7 +85,6 @@ app.controller("myController", function ($http) {
                     self.calculateAverage();
                 })
     };
-
     this.calculateAverage = function () {
         var total = 0;
         for (var i = 0; i < self.studentArray.length; i++) {
@@ -107,14 +93,12 @@ app.controller("myController", function ($http) {
         self.average = Math.floor(total / self.studentArray.length);
         console.log(self.average);
     };
-
     this.cancelClicked = function () {                            //clear the input forms
         this.studentName = "";
         this.studentCourse = "";
         this.studentGrade = "";
     };
 });
-
 app.controller("formController", function () {      //adding
 
 });
